7be5163e12f693b412c8d85ccf65d151e948493f
LucasSzejnberg <48111356@est.ort.edu.ar>
2024-06-06 14:28:55 -0300

    No se que hice

---
+++ src/HistoriaClinica.tsx
@@ -0,0 +1,12 @@
+// src/screens/HistoriaClinica.tsx
+import React from 'react';
+
+const HistoriaClinica = () => {
+  return (
+    <div>
+      <h1>Hola Mundo</h1>
+    </div>
+  );
+};
+
+export default HistoriaClinica;
