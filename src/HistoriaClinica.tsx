f137b4539271b52dbf47f588a21589825a87a332
LucasSzejnberg <48111356@est.ort.edu.ar>
2024-06-10 15:36:29 -0300

    Pantalla nueva con sidebar 2

--- src/HistoriaClinica.tsx
+++ src/HistoriaClinica.tsx
@@ -1,12 +1,17 @@
 // src/screens/HistoriaClinica.tsx
-
+import Sidebar2 from './components/Sidebar2';
 import BannerHistorial from './components/BannerHistorial'; // Ajusta la ruta según la ubicación de tu archivo
 
 const HistoriaClinica = () => {
   return (
     <div>
       <BannerHistorial logoSrc="/logo.png" altText="Mi Logo" />
-      <h1>Hol Mundo</h1>
+      <Sidebar2 
+        topButton1ImgSrc="/EstudiosBoton1.png"
+        topButton2ImgSrc="/HistoriaBoton1.png"
+        bottomButtonImgSrc="/HistorialNuevoBoton.png"
+        sidebarImgSrc="/LogoVertical.png"
+      />
     </div>
   );
 };
---
+++ src/components/Sidebar2.tsx
@@ -0,0 +1,56 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import './Sidebar2.css';
+import Modal2 from './Modal2';
+
+interface SidebarProps {
+  topButton1ImgSrc: string;
+  topButton2ImgSrc: string;
+  sidebarImgSrc: string;
+  bottomButtonImgSrc: string;
+}
+
+const Sidebar: React.FC<SidebarProps> = ({ 
+  topButton1ImgSrc, 
+  topButton2ImgSrc, 
+  sidebarImgSrc, 
+  bottomButtonImgSrc 
+}) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const navigate = useNavigate();
+
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
+  const handleButton1Click = () => {
+    navigate('/');
+  };
+
+  const handleButton2Click = () => {
+    navigate('/historia-clinica');
+  };
+
+  return (
+    <div className="sidebar2">
+      <button className="sidebar-btn" onClick={handleButton1Click}>
+        <img src={topButton1ImgSrc} alt="Button 1" className="btn-img" />
+      </button>
+      <button className="sidebar-btn" onClick={handleButton2Click}>
+        <img src={topButton2ImgSrc} alt="Button 2" className="btn-img" />
+      </button>
+      <img src={sidebarImgSrc} alt="Sidebar" className="sidebar-img" />
+      
+      <button className="sidebar-btn" onClick={openModal}>
+        <img src={bottomButtonImgSrc} alt="Bottom Button" className="btn-img" />
+      </button>
+      <Modal2 isOpen={isModalOpen} onClose={closeModal} />
+    </div>
+  );
+}
+
+export default Sidebar;
