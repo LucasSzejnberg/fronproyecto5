40b581a76b562f66b6428f879dbe2c3196b2efb6
Lucasszejn <48111356@est.ort.edu.ar>
2024-07-03 21:48:06 -0300

    Recargas de pantalla

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
-import { Routes, Route } from 'react-router-dom';
+
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HistoriaClinica from './HistoriaClinica';
 import Estudios from './Estudios';
-import Turnos from './Turnos'; // Importar el nuevo componente
+import Turnos from './Turnos'; // Asegúrate de importar el componente Turnos si lo necesitas
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Estudios />} />
-      <Route path="/historia-clinica" element={<HistoriaClinica />} />
-      <Route path="/turnos" element={<Turnos />} /> {/* Nueva ruta */}
-    </Routes>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Estudios />} />
+        <Route path="/historia-clinica" element={<HistoriaClinica />} />
+        <Route path="/turnos" element={<Turnos />} /> {/* Añade otras rutas según sea necesario */}
+      </Routes>
+    </Router>
   );
 }
 
