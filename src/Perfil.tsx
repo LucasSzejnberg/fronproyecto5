e487b96b3713b50aa1921ad1b165486f547603b7
Dantenishi <47700873@est.ort.edu.ar>
2024-09-23 11:09:31 -0300

    vuelta del viaje

--- src/Perfil.tsx
+++ src/Perfil.tsx
@@ -11,37 +11,37 @@ const Casa: React.FC = () => {
       </div>
 
       <div className="franja media">
-      <div className="todomedio">
-        <div className="franja-izquierda">
-          <img src="imagen.png" alt="Descripción de la imagen" />
-          <h2>DATOS PERSONALES</h2>
-          <input type="text" placeholder="Nombre" />
-          <input type="text" placeholder="Apellido" />
-          <input type="email" placeholder="Correo electrónico" />
-          <input type="date" placeholder="Fecha de nacimiento" />
-          <input type="text" placeholder="Sexo" />
-        </div>
-
-        <div className="franja-central">
-          <h2>DATOS FISICOS</h2>
-          <input type="text" placeholder="Peso" />
-          <input type="text" placeholder="Altura" />
-          <input type="text" placeholder="Tipo de sangre" />
-          <h2>DATOS MEDICOS</h2>
-          <input type="text" placeholder="Médico principal" />
-          <input type="text" placeholder="Número de mi credencial" />
-          <input type="text" placeholder="Obra social" />
-          <input type="text" placeholder="Plan obra social" />
-        </div>
-
-        <div className="franja-derecha">
-          <h2>ENFERMEDADES CRONICAS</h2>
-          <input type="text" placeholder="Enfermedades crónicas" />
-          <div className="botones">
-            <button>Guardar</button>
-            <button>Cerrar</button>
+        <div className="todomedio">
+          <div className="franja-izquierda">
+            <img src="imagen.png" alt="Descripción de la imagen" />
+            <h2>DATOS PERSONALES</h2>
+            <input type="text" className="input-estilo" placeholder="Nombre" />
+            <input type="text" className="input-estilo" placeholder="Apellido" />
+            <input type="email" className="input-estilo" placeholder="Correo electrónico" />
+            <input type="date" className="input-estilo" placeholder="Fecha de nacimiento" />
+            <input type="text" className="input-estilo" placeholder="Sexo" />
           </div>
-        </div>
+
+          <div className="franja-central">
+            <h2>DATOS FISICOS</h2>
+            <input type="text" className="input-estilo" placeholder="Peso" />
+            <input type="text" className="input-estilo" placeholder="Altura" />
+            <input type="text" className="input-estilo" placeholder="Tipo de sangre" />
+            <h2>DATOS MEDICOS</h2>
+            <input type="text" className="input-estilo" placeholder="Médico principal" />
+            <input type="text" className="input-estilo" placeholder="Número de mi credencial" />
+            <input type="text" className="input-estilo" placeholder="Obra social" />
+            <input type="text" className="input-estilo" placeholder="Plan obra social" />
+          </div>
+
+          <div className="franja-derecha">
+            <h2>ENFERMEDADES CRONICAS</h2>
+            <input type="text" className="input-estilo" placeholder="Enfermedades crónicas" />
+            <div className="botones">
+              <button>Guardar</button>
+              <button>Cerrar</button>
+            </div>
+          </div>
         </div>
       </div>
 
